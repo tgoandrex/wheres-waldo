8eaed7aa4834eb5802e87541bfb3a295aff6709d
tgoandrex <tgoandrex@gmail.com>
2023-01-19 21:00:59 -0600

    Modify package.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  app.use(express.static('client/build'));
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
+
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
