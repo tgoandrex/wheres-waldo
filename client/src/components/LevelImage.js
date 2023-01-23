1e7f9e87bac23c6a08f3e770422253e6fda9f191
tgoandrex <tgoandrex@gmail.com>
2023-01-23 17:49:28 -0600

    Add 'HTTP: to fetch routes

--- client/src/components/LevelImage.js
+++ client/src/components/LevelImage.js
@@ -7,7 +7,7 @@ function LevelImage(props) {
     const param = useParams();
 
     useEffect(() => {
-        fetch(`https://${window.location.hostname}:10000/coordinates/${param.level}`)
+        fetch(`http://${window.location.hostname}:10000/coordinates/${param.level}`)
         .then(res => res.json())
         .then(data => {
             setXCoord(data.x_coord);
